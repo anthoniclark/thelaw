dc406b87d8cc8bcd61f9db0b8c6ae7e512a5b2b9
Hardik Shah <samirmistry786@gmail.com>
2018-07-10 13:13:44 +0530

    comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  origin: 'http://spinfoweb-001-site1.ctempurl.com/api/'
+  origin: 'http://spinfoweb-001-site1.ctempurl.com/api/' // end point
 };
